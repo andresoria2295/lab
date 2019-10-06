e357a96ce870c488ce8146c18b37cdde3eb9ab86
andresoria2295 <andres.95.za@gmail.com>
2019-10-06 13:11:09 -0300

    Carga correcta de pagina. Falla en parseo

--- alumnos/55037-Andres-Soria/Trabajo_Practico_3/Trabajo_Practico_3.py
+++ alumnos/55037-Andres-Soria/Trabajo_Practico_3/Trabajo_Practico_3.py
@@ -1,69 +1,75 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 import socket
 import threading
+import os
 
 def parse(tamanio):
-    #Formulación de string de la petición a partir del tamaño en bytes.
+    #Formulacion de string de la peticion a partir del tamano en bytes.
     string = str(tamanio)
-    #División de cadena de caracteres (petición) en lista. Listado de string.
-    string = string.split("\\r\\n")
-    #Creación de lista solicitud.
-    solicitud = {}
-    for campo in string[1:-2]:
-        #Toma el campo y le aplica el metodo split para adquirir dirección.
-        valores = campo.split(":")
-        #Devolución de cadena de caracteres en minúscula de la cadena dada.
-        solicitud[valores[0].lower()] = valores[1]
-    return solicitud
+    #Division de cadena de caracteres (peticion) en lista. Listado de string.
+    #Toma de segundo elemento de la lista posteriormente de "Host: ".
+    string = string.split("Host: ")[1]
+    #Division de cadena en lista donde cada linea (renglon) es un elemento de la lista.
+    string = string.splitlines()[0]
+    #Devolucion de string previamente listado.
+    return string
 
 def manejador(cliente, direccion):
-    #Se recibe la petición del cliente.
+    #Se recibe la peticion del cliente.
     peticion = cliente.recv(1024000)
-    #Llamada a función parse para extraer dirección.
-    extraccion = parse(peticion)
-    #print (extraccion["host"][1:])
-    #Creación de socket para conectarse a servidor remoto (S).
+    #Llamada a funcion parse para extraer direccion.
+    #extraccion = parse(peticion)
+    #print (extraccion)
+    #Creacion de socket para conectarse a servidor remoto (S).
     servidor_remoto = socket.socket()
-    #Conección a servidor remoto.
-    servidor_remoto.connect((extraccion["host"][1:], 80))
-    #servidor_remoto.connect(("www.coto.com.ar", 80))
-    #Enviar petición a servidor remoto.
+    #Division de cadena extraccion en lista y toma del primer elemento de la lista.
+    #direccion_url = extraccion.split(":")[0]
+    #Division de cadena extraccion en lista y toma del segundo elemento en formato entero de la lista.
+    #Excepcion de fuera de rango -> puerto = 80
+    #puerto = int (extraccion.split(":")[1])
+    #Coneccion a servidor remoto.
+    #servidor_remoto.connect((direccion_url, puerto))
+    servidor_remoto.connect(("www.coto.com.ar", 80))
+    #Envio de peticion a servidor remoto.
     servidor_remoto.send(peticion)
-    #Tomar respuesta de servidor remoto.
-    respuesta = servidor_remoto.recv(1024)
-    #print(peticion)
-    #print("\n")
-    #Devolución de respuesta de la petición al cliente.
-    cliente.send(respuesta)
+
+    while True:
+        #Tomar respuesta de servidor remoto.
+        respuesta = servidor_remoto.recv(20480)
+        if not respuesta:
+ 	    break
+        #Devolucion de respuesta de la peticion al cliente.
+        cliente.send(respuesta)
+
     #Cierre de servidor remoto.
-    #servidor_remoto.close()
+    servidor_remoto.close()
     #Cierre de cliente.
-    #cliente.close()
+    cliente.close()
 
 # S - P - C
 
-#Creación de socket para comunicacion entre cliente (C) y proxy (P).
+#Creacion de socket para comunicacion entre cliente (C) y proxy (P).
 proxy_socket = socket.socket()
 proxy_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#Especificación de direccion del socket.
+#Especificacion de direccion del socket.
 proxy_socket.bind(("127.0.0.1", 8080))
 #Hacer que el servidor escuche a nuevas conexiones.
-proxy_socket.listen()
+proxy_socket.listen(5)
 
 while True:
-    #Aceptación de nueva conexión del cliente (C).
+    #Aceptacion de nueva conexion del cliente (C).
     cliente_socket, cliente_direccion = proxy_socket.accept()
-    #Creación de hilo con el nuevo socket para manejar y administrar la solicitud (petición).
+    #Creacion de hilo con el nuevo socket para manejar y administrar la solicitud (peticion).
     hilo_cliente = threading.Thread(target = manejador, args = (cliente_socket, cliente_direccion))
-    #Inicialización de hilo.
+    #Inicializacion de hilo.
     hilo_cliente.start()
 
 '''
-#Inicialización del analizador.
+#Inicializacion del analizador.
 parser = argparse.ArgumentParser()
 
-#Añadir argumentos al parse.
+#Anadir argumentos al parse.
 parser.add_argument("-f", "--file", dest = "ruta_archivo", nargs = 1, required = True, help = "Ruta de archivo de entrada")
 parser.add_argument("-p", "--port", dest = "puerto", nargs = "?", default = 8080, const = 8080, type = int, help = "Numero de puerto")
 
