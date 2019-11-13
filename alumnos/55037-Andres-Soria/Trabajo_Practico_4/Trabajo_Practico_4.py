b79a6fc6be2456122bcf1b4540f5c0b99a19d6d6
andresoria2295 <andres.95.za@gmail.com>
2019-11-12 23:10:07 -0300

    Bajada y conversión es imagenes a formato .ppm funcionando.

--- alumnos/55037-Andres-Soria/Trabajo_Practico_4/Trabajo_Practico_4.py
+++ alumnos/55037-Andres-Soria/Trabajo_Practico_4/Trabajo_Practico_4.py
@@ -25,14 +25,14 @@ argumentos = Parser_Argumento.parse_args()
 
 #Listado de sitios web ingresados y añadidos a la aplicación.
 URL_web = argumentos.direccion_web
-contenido = []
+#contenido = []
 links = []
 
 #Se recorre la lista de direcciones web.
 for URL in URL_web:
     #Uso de BeautifulSoup (Biblioteca de Python para analizar documentos html).
     #Conversión del contenido de cada sitio web de formato html a formato texto.
-    html = BeautifulSoup(requests.get(URL).text)
+    html = BeautifulSoup(requests.get(URL).text, "html.parser")
     #Búsqueda y localización de imagenes a partir de la etiqueta de filtrado "img".
     imagenes = html.findAll("img")
     #Se recorre la lista de imagenes halladas.
@@ -55,21 +55,54 @@ contador = 0
 
 #Se recorre la lista que contiene las direcciones de imagenes (absolutas).
 for link in links:
-    try:
-        #Uso de módulo urllib.request, que permite copiar un objeto de red denotado por una URL en un archivo local.
-        #Toma cada dirección de imagen absoluta de la lista, sitúa dicha imagen representada en una carpeta previamente asignada y se le atribuye un valor.
-        urllib.request.urlretrieve(link, "./descarga_imagenes/" + str(contador))
-        contador = contador + 1
-        #Se carga la imagen localizada en la carpeta que le fue asignada y se le atribuye un valor.
-        imagen = Image.open("./descarga_imagenes/" + str(contador))
-        #Se guarda dicha imagen en la misma carpeta que le fue asignada anteriormente, con el mismo valor, pero en formato ".ppm".
-        imagen.save("./descarga_imagenes/" + str(contador) + ".ppm")
-    #Manejo de errores.
-    #Uso de módulo urllib.error para excepciones generadas por el módulo urllib.request.
-    #HTTPError es útil cuando se manejan errores de HTTP exóticos, como solicitudes de autenticación.
-    except urllib.error.HTTPError as error:
-         #error.reason fundamenta la razón del error. Puede ser una cadena de mensaje u otra instancia de excepción.
-         print("No es posible descargar imagen: " + error.reason)
-    #OSError es aplicado cuando una función devuelve un error relacionado con el sistema.
-    except OSError as error:
-        print("OSError: " + str(error))
+    #Se cuenta internamente cada dirección de imagen.
+    cantidad = len(link)
+    #Si la imagen corresponde a formato ".png":
+    if (link[cantidad-3::] == "png"):
+        try:
+            #Uso de módulo urllib.request, que permite copiar un objeto de red denotado por una URL en un archivo local.
+            #Toma cada dirección absoluta de imagen de la lista, sitúa dicha imagen representada en una carpeta previamente asignada y se le atribuye un valor y formato.
+            urllib.request.urlretrieve(link, "./descarga_imagenes/" + str(contador) + ".png")
+            #Designación de ruta/dirección de la imagen.
+            ruta = 'descarga_imagenes/' + str(contador)
+            #Cargado de imagen, conversión a RGB y posteriormente guardado en ruta previamente declarada en formato ".ppm".
+            imagen = Image.open(ruta + ".png").convert('RGB').save(ruta + ".ppm")
+            #Eliminación de imagen de previo formato descargado. (".png")
+            os.remove(ruta + ".png")
+            contador = contador + 1
+        #Manejo de errores.
+        #Uso de módulo urllib.error para excepciones generadas por el módulo urllib.request.
+        #HTTPError es útil cuando se manejan errores de HTTP exóticos, como solicitudes de autenticación.
+        except urllib.error.HTTPError as error:
+            #error.code manifiesta el código y tipo de error.
+            print('Estado: ', error.code)
+             #error.reason fundamenta la razón del error. Puede ser una cadena de mensaje u otra instancia de excepción.
+            print('Causa/Motivo: ', error.reason)
+        #OSError es aplicado cuando una función devuelve un error relacionado con el sistema.
+        except OSError as error:
+            print("OSError: " + str(error))
+
+    #Si la imagen corresponde a formato ".jpg":
+    if (link[cantidad-3::] == "jpg"):
+        try:
+            #Uso de módulo urllib.request, que permite copiar un objeto de red denotado por una URL en un archivo local.
+            #Toma cada dirección absoluta de imagen de la lista, sitúa dicha imagen representada en una carpeta previamente asignada y se le atribuye un valor y formato.
+            urllib.request.urlretrieve(link, "./descarga_imagenes/" + str(contador) + ".jpg")
+            #Designación de ruta/dirección de la imagen.
+            ruta = 'descarga_imagenes/'+str(contador)
+            #Cargado de imagen, conversión a RGB y posteriormente guardado en ruta previamente declarada en formato ".ppm".
+            imagen = Image.open(ruta + ".jpg").convert('RGB').save(ruta + ".ppm")
+            #Eliminación de imagen de previo formato descargado. (".jpg")
+            os.remove(ruta + ".jpg")
+            contador = contador + 1
+        #Manejo de errores.
+        #Uso de módulo urllib.error para excepciones generadas por el módulo urllib.request.
+        #HTTPError es útil cuando se manejan errores de HTTP exóticos, como solicitudes de autenticación.
+        except urllib.error.HTTPError as error:
+            #error.code manifiesta el código y tipo de error.
+            print('Estado: ', error.code)
+             #error.reason fundamenta la razón del error. Puede ser una cadena de mensaje u otra instancia de excepción.
+            print('Causa/Motivo: ', error.reason)
+        #OSError es aplicado cuando una función devuelve un error relacionado con el sistema.
+        except OSError as error:
+            print("OSError: " + str(error))
